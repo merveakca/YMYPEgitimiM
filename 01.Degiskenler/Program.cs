2fec1b872c469dff6371d08cc9e5c1c04769fac9
merveakca <smkirac@gmail.com>
2023-07-16 15:27:39 +0300

    .

--- 01.Degiskenler/Program.cs
+++ 01.Degiskenler/Program.cs
@@ -1,24 +1,24 @@
 ﻿Console.WriteLine("Hello World!");
 
-//Bazı değişkenler aşağıdadır.
+//Bazı değişken tipleri aşağıdadır.
 //string, int, decimal, double, float, bool, DateTime, Object
 
-//değişkenlere ilk değer ataması yapılmadığında değerleri aşağıdaki gibidir. Değişkenlerin altını yeşil çizdiğinde o değişken henüz bir yerde kullanılmamış demektir.
-
+//değişkenlere ilk değer ataması yapılmadığında değerleri aşağıdaki gibidir. Değişkenlerin altının yeşil çizdiğinde, o değişken henüz bir yerde kullanılmamış demektir.
 string name;     //null
 int age;         //0
 bool isActive;   //false
 
-float n1 = 100;
+//float tipinde bir değişken tanımladığımızda ve ondalıklı bir değer verdiğimizde (sistem bunu double olarak kabul eder) sistem, implicit dönüşüm (bilinçsiz dönüşüm) yapılamadığı için hata verir. Bunu engellemek için explicit (bilinçli) olarak aşağıdaki gibi dönüşüm yapmamız gerekir.
+float n1 = (float)198.7;
 int n2 = 200;
 float n3 = n1 + n2;
 Console.WriteLine(n3);
 
 string nameLastName = "Merve Akça";
-
 name = "asdasd";
-
 char karakter = 'A';
+
+//Satırı aşağıya kaydırmak için; yukarıdaki araç çubuğundan şu sırayı takip edin: Tools/Options/Text Editor/All Languages/Word wrap(seçeneğini aktifleştirin)
 string aciklama = "asdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd";
 
 Console.WriteLine(nameLastName);
