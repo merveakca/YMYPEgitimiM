74a1c94cc547e5eb40d38d8a4b7b992a2924ae04
merveakca <smkirac@gmail.com>
2023-07-16 15:22:11 +0300

    .

--- 01.Degiskenler/Program.cs
+++ 01.Degiskenler/Program.cs
@@ -1,16 +1,18 @@
 ﻿Console.WriteLine("Hello World!");
 
+//Bazı değişkenler aşağıdadır.
+//string, int, decimal, double, float, bool, DateTime, Object
 
-//string,int,decimal,double,float,bool,DateTime,Object
+//değişkenlere ilk değer ataması yapılmadığında değerleri aşağıdaki gibidir. Değişkenlerin altını yeşil çizdiğinde o değişken henüz bir yerde kullanılmamış demektir.
 
-string name; //null
-int age; //0
-bool isActive; //false
+string name;     //null
+int age;         //0
+bool isActive;   //false
 
 float n1 = 100;
 int n2 = 200;
-var n3 = n1 + n2;
-Console.WriteLine(n3.GetType());
+float n3 = n1 + n2;
+Console.WriteLine(n3);
 
 string nameLastName = "Merve Akça";
 
