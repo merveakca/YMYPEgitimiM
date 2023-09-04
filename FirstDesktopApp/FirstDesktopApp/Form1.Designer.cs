cf941eeffc9f29df34e10f079efc3831be48aeb9
merveakca <smkirac@gmail.com>
2023-09-04 04:01:38 +0300

    .

---
+++ FirstDesktopApp/FirstDesktopApp/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace FirstDesktopApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Öğrenci", "Öğretmen", "Veli", "Müdür", "Müdür Yardımcısı" });
+            comboBox1.Location = new Point(519, 222);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1582, 960);
+            Controls.Add(comboBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+    }
+}
---
+++ FirstDesktopApp/FirstDesktopApp/HesapMakinesi.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace FirstDesktopApp
+{
+    partial class HesapMakinesi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnOne = new Button();
+            btnTwo = new Button();
+            btnThree = new Button();
+            btnFour = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            SuspendLayout();
+            // 
+            // btnOne
+            // 
+            btnOne.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            btnOne.Location = new Point(242, 299);
+            btnOne.Name = "btnOne";
+            btnOne.Size = new Size(172, 166);
+            btnOne.TabIndex = 0;
+            btnOne.Text = "1";
+            btnOne.UseVisualStyleBackColor = true;
+            btnOne.Click += button1_Click;
+            // 
+            // btnTwo
+            // 
+            btnTwo.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            btnTwo.Location = new Point(434, 299);
+            btnTwo.Name = "btnTwo";
+            btnTwo.Size = new Size(172, 166);
+            btnTwo.TabIndex = 1;
+            btnTwo.Text = "2";
+            btnTwo.UseVisualStyleBackColor = true;
+            btnTwo.Click += button2_Click;
+            // 
+            // btnThree
+            // 
+            btnThree.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            btnThree.Location = new Point(623, 299);
+            btnThree.Name = "btnThree";
+            btnThree.Size = new Size(172, 166);
+            btnThree.TabIndex = 2;
+            btnThree.Text = "3";
+            btnThree.UseVisualStyleBackColor = true;
+            btnThree.Click += button3_Click;
+            // 
+            // btnFour
+            // 
+            btnFour.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            btnFour.Location = new Point(242, 487);
+            btnFour.Name = "btnFour";
+            btnFour.Size = new Size(172, 166);
+            btnFour.TabIndex = 3;
+            btnFour.Text = "4";
+            btnFour.UseVisualStyleBackColor = true;
+            btnFour.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(434, 487);
+            button5.Name = "button5";
+            button5.Size = new Size(172, 166);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button6.Location = new Point(623, 487);
+            button6.Name = "button6";
+            button6.Size = new Size(172, 166);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button7.Location = new Point(242, 676);
+            button7.Name = "button7";
+            button7.Size = new Size(172, 166);
+            button7.TabIndex = 6;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button8.Location = new Point(434, 676);
+            button8.Name = "button8";
+            button8.Size = new Size(172, 166);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button9.Location = new Point(623, 676);
+            button9.Name = "button9";
+            button9.Size = new Size(172, 166);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button10.Location = new Point(1216, 270);
+            button10.Name = "button10";
+            button10.Size = new Size(159, 186);
+            button10.TabIndex = 9;
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button11.Location = new Point(434, 864);
+            button11.Name = "button11";
+            button11.Size = new Size(172, 166);
+            button11.TabIndex = 10;
+            button11.Text = "0";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button12.Location = new Point(1216, 462);
+            button12.Name = "button12";
+            button12.Size = new Size(159, 186);
+            button12.TabIndex = 11;
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button13.Location = new Point(1216, 654);
+            button13.Name = "button13";
+            button13.Size = new Size(159, 186);
+            button13.TabIndex = 12;
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button14.Location = new Point(1216, 846);
+            button14.Name = "button14";
+            button14.Size = new Size(159, 186);
+            button14.TabIndex = 13;
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // HesapMakinesi
+            // 
+            AutoScaleDimensions = new SizeF(18F, 45F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1589, 1158);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(btnFour);
+            Controls.Add(btnThree);
+            Controls.Add(btnTwo);
+            Controls.Add(btnOne);
+            Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(5);
+            Name = "HesapMakinesi";
+            Text = "HesapMakinesi";
+            Load += HesapMakinesi_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnOne;
+        private Button btnTwo;
+        private Button btnThree;
+        private Button btnFour;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+    }
+}
